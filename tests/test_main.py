56c9e2c35b16e65253c3589b8620b0293821a9a0
farazjawedd <101464414+farazjawedd@users.noreply.github.com>
2023-09-18 08:51:25 +0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,33 @@
-from sorc.main import sanity_add
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def test_sanity_add():
-  assert sanity_add(2, 3) == 5
+# sys.path.append("/workspaces/fj49_week2_ds")
+from sorc.main import mean, median, mode, std, viz, reader
+
+spotify = reader()
+mean = mean()
+median = median()
+mode = mode()
+std = std()
+viz = viz()
+
+
+def test_mean():
+    assert mean == int(spotify["duration_ms"].mean())
+
+
+def test_median():
+    assert median == int(spotify["duration_ms"].median())
+
+
+def test_mode():
+    assert mode == int(spotify["duration_ms"].mode())
+
+
+def test_std():
+    assert std == int(spotify["duration_ms"].std())
+
+
+def test_viz():
+    assert viz is not None  # asserts that viz is not empty
